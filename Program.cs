9fb48a2e893c2abd5e5b03e8f1a18df0576d1915
jwbnw <32151160+jwbnw@users.noreply.github.com>
2023-09-23 22:48:56 -0700

    #4: Inject auth service

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 using System.Text;
+using Web3Ai.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddSingleton<UTF8Encoding>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
