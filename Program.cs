fa1586756e20b9eb9403a10e06ef4b9de7dcf099
jwbnw <32151160+jwbnw@users.noreply.github.com>
2023-09-24 23:40:23 -0700

    #4: Reigster new deps

--- Program.cs
+++ Program.cs
@@ -1,13 +1,28 @@
 using System.Text;
+using Web3Ai.Service.Services;
+using Web3Ai.Service.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Web3Ai.Service.Authorization;
 using Web3Ai.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//DI
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IJwtUtils, JwtUtils>();
+builder.Services.AddScoped<IUserDataService, UserDataService>();
+
 builder.Services.AddSingleton<UTF8Encoding>();
 builder.Services.AddControllers();
+
+// Db Context
+builder.Services.AddDbContext<UserContext>(opt => opt.UseInMemoryDatabase("Users"));
+
+// strongly typed settings object
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,6 +39,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<JwtMiddleware>();
 
 app.MapControllers();
 
