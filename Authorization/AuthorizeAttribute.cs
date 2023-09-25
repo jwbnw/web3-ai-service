7d6f4f6d9d7870d3c69878cb48ea81ba681c0412
jwbnw <32151160+jwbnw@users.noreply.github.com>
2023-09-24 23:36:33 -0700

    #4: Adds Auth

---
+++ Authorization/AuthorizeAttribute.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Web3Ai.Service.Data.Entities;
+
+namespace Web3Ai.Service.Authorization;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        // skip authorization if action is decorated with [AllowAnonymous] attribute
+        var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
+        if (allowAnonymous)
+            return;
+
+        // authorization
+        var user = (User?)context.HttpContext.Items["User"];
+        if (user == null)
+        {
+            // not logged in or role not authorized
+            context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+        }
+    }
+}
---
+++ Authorization/JwtMiddleware.cs
@@ -0,0 +1,26 @@
+namespace Web3Ai.Service.Authorization;
+
+using Web3Ai.Service.Services;
+
+public class JwtMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public JwtMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context, IUserDataService userDataService, IJwtUtils jwtUtils)
+    {
+        var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+        var userId = jwtUtils.ValidateJwtToken(token);
+        if (userId != null)
+        {
+            // attach user to context on successful jwt validation
+               context.Items["UserId"] = userDataService.GetById(userId.Value); // needs to be a guid
+        }
+
+        await _next(context);
+    }
+}
