94278406ab838a5cde4ded9460a957fd44ce19af
jwbnw <32151160+jwbnw@users.noreply.github.com>
2023-09-25 21:00:52 -0700

    #4: extend swagger

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using Web3Ai.Service.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Web3Ai.Service.Authorization;
 using Web3Ai.Service;
+using Web3Ai.Service.Utils;
+using Swashbuckle.AspNetCore.Annotations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +19,7 @@ builder.Services.AddScoped<IUserDataService, UserDataService>();
 builder.Services.AddSingleton<UTF8Encoding>();
 builder.Services.AddControllers();
 
-// Db Context
+// Db Context (Need to swap out for real db before too long)
 builder.Services.AddDbContext<UserContext>(opt => opt.UseInMemoryDatabase("Users"));
 
 // strongly typed settings object
@@ -25,7 +27,11 @@ builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSet
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.EnableAnnotations();
+});
 
 var app = builder.Build();
 
