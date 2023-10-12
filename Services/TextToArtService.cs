cc07a63c1ea003db0b618ecdd26fd3eb79e46595
jwbnw <32151160+jwbnw@users.noreply.github.com>
2023-10-11 23:02:02 -0700

    #3: adds to ai art svc

--- Services/TextToArtService.cs
+++ Services/TextToArtService.cs
@@ -1,14 +1,11 @@
 
 using Web3Ai.Service.Models;
-using Web3Ai.Service.Authorization;
-using Microsoft.Extensions.Caching.Memory;
-using Web3Ai.Service.Utils;
-using System.Net.Http;
+using Web3Ai.Service.Models.TextToArt;
 using System.Text.Json;
 using static System.Net.Mime.MediaTypeNames;
 using System.Text;
-using System.Diagnostics;
-using System.ComponentModel.DataAnnotations;
+
+using System.Net.Http.Headers;
 
 namespace Web3Ai.Service.Services;
 
@@ -20,22 +17,54 @@ public interface ITextToArtService
 public class TextToArtService : ITextToArtService
 {
 
-    public TextToArtService()
+    public TextToArtService(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
     }
 
     async Task<TextToArtTransactionResult> ITextToArtService.GenerateArt(ValidatedTextToArtTranscationRequest transcationRequest)
     {
-        return await MockResult();
-    }
+        // esstablish http client
+        var stablityHttpClient = _httpClientFactory.CreateClient("StablityClient");
 
-    private async Task<TextToArtTransactionResult> MockResult()
-    {
-        return new TextToArtTransactionResult()
-        {
-            Tx = ""
-        };
+        // make text to art request.
+        var postData = new StringContent(
+            JsonSerializer.Serialize(transcationRequest),
+            Encoding.UTF8,
+            Application.Json
+        );
+
+        stablityHttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("ValueHere");
+
+        //try for debugging  top level will handle
+ 
+            var response = await stablityHttpClient.PostAsync("https://api.stablity.ai/v1/generation/stable-diffusion-x1-1024-v1-0/text-to-image", postData);
+            var rawContent = await response.Content.ReadAsStringAsync();
+
+            var stablityTextToArtResponse = JsonSerializer.Deserialize<StablityTextToArtResponse>(rawContent);
+
+            var finalList = new TextToArtTransactionResult();
+
+            if(stablityTextToArtResponse?.StablityTextToArtImages == null) throw new ArgumentNullException("StablityTextToArtImages");
+
+
+            foreach(var value in stablityTextToArtResponse.StablityTextToArtImages)
+            {
+                var finalImage = new TextToArtImage()
+                {
+                    Base64 = value.Base64,
+                    FinshReason = value.FinshReason,
+                    Seed = value.Seed
+                };
+
+                finalList?.StablityTextToArtImages?.Add(finalImage);
+            }           
+            
+            if (finalList == null) throw new ArgumentNullException("finalList"); // clean this up
+            
+            return finalList; 
     }
+    private readonly IHttpClientFactory _httpClientFactory;
 }
 
 
