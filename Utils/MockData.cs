6793e5d66f831f33211cf1df109e9f209090ce8d
jwbnw <32151160+jwbnw@users.noreply.github.com>
2023-10-14 14:31:51 -0700

    make mock data a static class

--- Utils/MockData.cs
+++ Utils/MockData.cs
@@ -1,7 +1,7 @@
 
 namespace Web3Ai.Service.Utils;
 
-public class MockData 
+public static class MockData 
 {
- public readonly string MockStablityTextToArtResponse = """{"artifacts":[{"base64":"NE1yXL9wKg/7ISKRTARJs0HeDfSPjI6sT7NscWl+qdnw4lVXKnNWnKACgGVjxtasHVu/6dDUIaRu4/oXWlPOpKqm4WyZ0IJacALEZBwC5FZUXZdty4wvPdwYkcDMh/cggZlZJRNgghuFEOzUckr9sZtq/d8oBPKuBZz/BBfrwLEG0rErEqQAfOf2hajy2Js/6k1nM/BDAMjMBDYklIQ8YJjnOakdv0F+5TVXfeZj9/zNB9/UsB/6ld94U3/MJuljGIhV40Hko0SYHGCEFYB4M9vHPDtkZyfeNbecJB7sSgRcN00/lU4Q9ZlgTa2bo1nQAAAABJRU5ErkJggg==","seed":3303980766,"finishReason":"SUCCESS"}]}""";
+ public static readonly string MockStablityTextToArtResponse = """{"artifacts":[{"base64":"NE1yXL9wKg/7ISKRTARJs0HeDfSPjI6sT7NscWl+qdnw4lVXKnNWnKACgGVjxtasHVu/6dDUIaRu4/oXWlPOpKqm4WyZ0IJacALEZBwC5FZUXZdty4wvPdwYkcDMh/cggZlZJRNgghuFEOzUckr9sZtq/d8oBPKuBZz/BBfrwLEG0rErEqQAfOf2hajy2Js/6k1nM/BDAMjMBDYklIQ8YJjnOakdv0F+5TVXfeZj9/zNB9/UsB/6ld94U3/MJuljGIhV40Hko0SYHGCEFYB4M9vHPDtkZyfeNbecJB7sSgRcN00/lU4Q9ZlgTa2bo1nQAAAABJRU5ErkJggg==","seed":3303980766,"finishReason":"SUCCESS"}]}""";
 }
